7e13d2f5a183c610c70dfe0f63b78631a6187033
josantcogsci <ja.mendozachavez@gmail.com>
2020-06-08 22:10:09 -0500

    count_lines

---
+++ count_lines.c
@@ -0,0 +1,22 @@
+/**
+ * ' Count Lines '
+ * Written by José Antonio Mendoza Chávez
+ * Monday, June 8th, 2020
+ * E-mail: ja.mendozachavez@gmail.com
+ * Github: https://github.com/josantcogsci
+ * LinkedIn: https://www.linkedin.com/in/josantcogsci
+ */
+
+#include <stdio.h>
+
+/* count lines in input */
+int main(void)
+{
+    int c, nl;
+
+    nl = 0;
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf("%d\n", nl);
+}
